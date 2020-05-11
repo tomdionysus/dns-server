b50393bca9f7457f8347603f29b97cbf886ad441
Tom Cully <tom@blackraven.co.nz>
2020-05-11 20:06:19 +1200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const Logger = require('./lib/Logger')
+const DNSServer = require('./lib/DNSServer')
+
+function main() {
+	// ENV and defaults
+
+	// Logger
+	var logger = new Logger()
+
+	// Boot Message
+	logger.log('dns-server (Tom Cully)','----')
+	logger.log('v1.0.0','----')
+	logger.log('','----')
+	logger.log('Logging Level %s','----', Logger.logLevelToString(logger.logLevel))
+
+	// Main Downloader
+	var svr = new DNSServer({
+		logger: logger,
+		env: process.env.ENV || 'prod',
+		port: parseInt(process.env.PORT) || 54
+	})
+
+	var handle = ()=>{
+		logger.debug("SIGTERM/SIGINT Received - Stopping")
+		svr.stop()
+		logger.info("Stopped")
+		process.exit()
+	}
+
+	process.on('SIGTERM', handle)
+	process.on('SIGINT', handle)
+
+	// Downloader Start
+	svr.start()
+
+}
+
+main()

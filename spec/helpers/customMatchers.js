9ca2a2f26ac487b373620670a3929afec15d4692
Tom Cully <tom@blackraven.co.nz>
2020-05-13 15:24:13 +1200

    JSStandard

--- spec/helpers/customMatchers.js
+++ spec/helpers/customMatchers.js
@@ -3,4 +3,4 @@
 jasmine.anyUUID = jasmine.stringMatching(/[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}/)
 jasmine.anyHash256 = jasmine.stringMatching(/[0-9a-f]{32}/)
 jasmine.anyHash512 = jasmine.stringMatching(/[0-9a-f]{64}/)
-jasmine.anyHash256Prefix = (prefix) => { return jasmine.stringMatching(new RegExp(prefix+'[0-9a-f]{32}')) }
+jasmine.anyHash256Prefix = (prefix) => { return jasmine.stringMatching(new RegExp(prefix + '[0-9a-f]{32}')) }
